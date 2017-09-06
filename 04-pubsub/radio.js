56bc61820680df107575d65c27d8977a4b4f3879
Magesh <tkmagesh77@gmail.com>
2017-09-06 17:00:58 +0530

    radio.js updated

--- 04-pubsub/radio.js
+++ 04-pubsub/radio.js
@@ -10,26 +10,38 @@ var radio = (function(){
 		this.__event_name__ = event_name;
 	}
 
-	Radio.prototype.subscribe = function(subscriptionFn){
+	Radio.prototype.subscribe = function(){
 		this.__event_subscriptions__ = this.__event_subscriptions__ || [];
-		this.__event_subscriptions__.push(subscriptionFn);
+		Array.prototype.forEach.call(arguments, function(subscription){
+			this.__event_subscriptions__.push(subscription);
+		}.bind(this));
 		return this;
 		
 	}
 
-	Radio.prototype.broadcast = function(data){
+	Radio.prototype.broadcast = function(){
+		var args = arguments;
 		var subscriptions = this.__event_subscriptions__ || [];
 		subscriptions.forEach(function(subscription){
-			subscription(data);
-		});
+			if (typeof subscription === 'function'){
+				subscription.apply(null, args)
+			}
+			if (Array.isArray(subscription)){
+				var fn = subscription[0],
+					context = subscription[1];
+				fn.apply(context, args);
+			}
+		}.bind(this));
 		return this;
 	}
 
-	Radio.prototype.unsubscribe = function(subscriptionFn){
+	Radio.prototype.unsubscribe = function(){
 		var subscriptions = this.__event_subscriptions__ || [];
-		let index = subscriptions.indexOf(subscriptionFn);
-		if (index !== -1)
-			subscriptions.splice(index, 1);
+		Array.prototype.forEach.call(arguments, function(subscriptionFn){
+			let index = subscriptions.indexOf(subscriptionFn);
+			if (index !== -1)
+				subscriptions.splice(index, 1);
+		}.bind(this));
 		return this;
 	}
 	return radio;
